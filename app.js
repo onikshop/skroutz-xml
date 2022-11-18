a2dfb21fd9125aca7c1058b9c517660ab152e7d2
onikshop <118123625+onikshop@users.noreply.github.com>
2022-11-18 11:31:06 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+const app = express()
+app.use(express.static('public'))
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log('Server is running on port 3000 !')
+})
